94738c39d872f5c0dc696f14056d3bfdb27cae42
Aliaksandr <mast.muha@gmail.com>
2025-06-04 16:25:45 +0300

    ai v1.0

--- aipredheat.py
+++ aipredheat.py
@@ -3,9 +3,11 @@ import joblib
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 import pandas as pd
 import numpy as np
+import tensorflow as tf
 from sklearn.preprocessing import StandardScaler, MinMaxScaler, RobustScaler
 from sklearn.model_selection import train_test_split
 from keras.models import Sequential
+from keras.regularizers import l2
 from keras.layers import Dense, Dropout, BatchNormalization
 from keras.callbacks import EarlyStopping
 from datetime import datetime
@@ -39,6 +41,17 @@ heat_end2324 = pd.to_datetime('29.04.2024', dayfirst=True)
 heat_start2425 = pd.to_datetime('01.10.2024', dayfirst=True)
 heat_end2425 = pd.to_datetime('15.04.2025', dayfirst=True)
 
+heat_off_months = [
+    heat_end2223.month,
+    heat_end2324.month,
+    heat_end2425.month,
+]
+
+heat_on_months = [
+    heat_start2324.month,
+    heat_start2425.month,
+]
+
 # --- Загрузка данных для обучения ---
 data = pd.read_csv('db15gvs.csv', delimiter=';', decimal=',')
 
@@ -46,7 +59,7 @@ data = pd.read_csv('db15gvs.csv', delimiter=';', decimal=',')
 data['datetime'] = [f'{data["date"][i]} {data["time"][i]}' for i in range(len(data['date']))]
 data['datetime'] = data['datetime'].apply(lambda x: datetime.strptime(x, '%d.%m.%Y %H:%M:%S'))
 data['minute'] = data['datetime'].dt.minute
-data['month'] = data['datetime'].dt.minute
+data['month'] = data['datetime'].dt.month
 
 # --- Создание базы для обучения ---
 
@@ -63,17 +76,23 @@ new_data['is_heatperiod'] = (((new_data['datetime'] >= heat_start2324) & (new_da
                              | ((new_data['datetime'] >= heat_start2425) & (new_data['datetime'] <= heat_end2425))
                              | ((new_data['datetime'] <= heat_end2223)))
 
-new_data['hour'] = new_data['datetime'].dt.hour
+new_data['is_heatoffmonth'] = new_data['month'].apply(lambda m: int(m in heat_off_months))
+new_data['is_heatonmonth'] = new_data['month'].apply(lambda m: int(m in heat_on_months))
+new_data = new_data[new_data['month'] != 4].reset_index(drop=True)
 
-new_data['date_only'] = new_data['datetime'].dt.date    #Удаление дат где не было измерений в течении всего дня
+new_data['hour'] = new_data['datetime'].dt.hour
+z_scores = np.abs(stats.zscore(new_data['q']))
+new_data = new_data[z_scores < 3]
+
+new_data['date_only'] = new_data['datetime'].dt.date
 counts_per_day = new_data['date_only'].value_counts()
 valid_dates = counts_per_day[counts_per_day == 24].index
 new_data = new_data[new_data['date_only'].isin(valid_dates)].reset_index(drop=True)
 new_data.drop(columns=['date_only'], inplace=True)
 
 # --- Масштабирование данных для обучения ИИ ---
-scaler_tair = RobustScaler()
-scaler_y = RobustScaler()
+scaler_tair = StandardScaler()
+scaler_y = MinMaxScaler()
 scaler_day = StandardScaler()
 scaler_hour = StandardScaler()
 scaler_month = StandardScaler()
@@ -90,14 +109,14 @@ new_data['week_cos'] = np.cos(2 * np.pi * new_data['day_of_week'] / 7)
 new_data['hour_sin'] = (np.sin(2 * np.pi * new_data['hour']  / 24))
 new_data['hour_cos'] = (np.cos(2 * np.pi * new_data['hour']  / 24))
 
-new_data = new_data[new_data['is_heatperiod'] == False].reset_index(drop=True) #False - обучение для межотопительного, True - отопительного
+#new_data = new_data[new_data['is_heatperiod'] == False].reset_index(drop=True) #False - обучение для межотопительного, True - отопительного
 
 # --- Сохранение данных для обучения в excel ---
 with pd.ExcelWriter('mlp_datalearn.xlsx') as writer:
     new_data.to_excel(writer, sheet_name='Data', index=False)
 
 # --- Подготовка входных и выходных данных для обучения ИИ ---
-features = ['tair_sc', 'day_sc', 'hour_sin', 'hour_cos', 'hour_sc', 'is_weekend', 'month_sc',]
+features = ['tair_sc', 'day_sc', 'hour_sin', 'hour_cos', 'hour_sc', 'week_sin', 'week_cos', 'is_weekend', 'month_sc', 'is_heatoffmonth', 'is_heatonmonth']
 X = new_data[features].astype(np.float32).values
 y = new_data['q_sc'].values
 
@@ -105,14 +124,22 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, shuffle
 
 # --- Модель обучения ---
 model = Sequential()
-model.add(Dense(512, activation='relu', input_dim=X.shape[1]))
-model.add(Dense(1))
+model.add(Dense(512, activation='relu', input_dim=X.shape[1], kernel_regularizer=l2(0.001)))
+model.add(Dropout(0.4))
+model.add(Dense(256, activation='relu', kernel_regularizer=l2(0.001)))
+model.add(Dropout(0.3))
+model.add(Dense(128, activation='relu', kernel_regularizer=l2(0.001)))
+model.add(Dropout(0.2))
+model.add(Dense(64, activation='relu', kernel_regularizer=l2(0.001)))
+model.add(Dropout(0.1))
+model.add(Dense(1))  # последний слой без регуляризации
+
 model.compile(optimizer='adam', loss='mse')
 
 # --- Обучение ---
-early_stopping = EarlyStopping(monitor='val_loss', patience=20)
+early_stopping = EarlyStopping(monitor='val_loss', patience=50)
 history = model.fit(X_train, y_train,
-                    epochs=100,
+                    epochs=300,
                     batch_size=512,
                     validation_data=(X_test, y_test),
                     callbacks=[early_stopping],
@@ -126,9 +153,9 @@ joblib.dump({'scaler_tair': scaler_tair, 'scaler_day': scaler_day,
             'scalers_mlp_heatpredict.pkl')
 
 # --- Визуализация обучения и работы модели ---
-y_pred = model.predict(X)
+y_pred = model.predict(X_test)
 y_pred_real = scaler_y.inverse_transform(y_pred).flatten()
-y_test_real = scaler_y.inverse_transform(y.reshape(-1, 1)).flatten()
+y_test_real = scaler_y.inverse_transform(y_test.reshape(-1, 1)).flatten()
 
 # --- Ошибка ---
 mse = mean_squared_error(y_test_real, y_pred_real)
@@ -167,11 +194,12 @@ plt.plot([i for i in range(len(y_pred_real))], y_pred_real, linestyle='',
 
 plt.show()
 
+
 result = {
     'date': new_data['datetime'].dt.date,
     'time': new_data['datetime'].dt.time,
     'qreal': new_data['q'],
-    'qai': y_pred_real,
+    'qai': scaler_y.inverse_transform(model.predict(X)).flatten(),
 
 }
 
--- aipredwork.py
+++ aipredwork.py
@@ -15,7 +15,7 @@ from keras.regularizers import l2
 from keras.models import load_model
 
 
-# --- Загрузка обученной модели ---
+# --- Загрузка обученной модели и масшабирования ---
 model = load_model('model_mlp_heatpredict.keras')
 scaler = joblib.load('scalers_mlp_heatpredict.pkl')
 scaler_day = scaler['scaler_day']
@@ -23,4 +23,48 @@ scaler_hour = scaler['scaler_hour']
 scaler_tair = scaler['scaler_tair']
 scaler_y = scaler['scaler_y']
 scaler_month = scaler['scaler_month']
+
+# --- Загрузка исходных данных и обработка ---
+data = pd.read_csv('test.csv', delimiter=';', decimal=',')
+data['datetime'] = [f'{data["date"][i]} {data["time"][i]}' for i in range(len(data['date']))]
+data['datetime'] = data['datetime'].apply(lambda x: datetime.strptime(x, '%d.%m.%Y %H:%M:%S'))
+
+data['month'] = data['datetime'].dt.month
+data['hour'] = data['datetime'].dt.hour
+data['day_of_week'] = data['datetime'].dt.dayofweek
+
+# --- Получение признаков ---
+data['is_heatperiod'] = 0
+data['is_weekend'] = 0
+data['is_heatoffmonth'] = 0
+data['is_heatonmonth'] = 0
+
+data['tair_sc'] = scaler_tair.transform(data[['tair']])
+data['day_sc'] = scaler_day.transform(data[['day_of_week']])
+data['hour_sc'] = scaler_hour.transform(data[['hour']])
+data['month_sc'] = scaler_month.transform(data[['month']])
+
+data['week_sin'] = np.sin(2 * np.pi * data['day_of_week'] / 7)
+data['week_cos'] = np.cos(2 * np.pi * data['day_of_week'] / 7)
+data['hour_sin'] = (np.sin(2 * np.pi * data['hour']  / 24))
+data['hour_cos'] = (np.cos(2 * np.pi * data['hour']  / 24))
+
+features = ['tair_sc', 'day_sc', 'hour_sin', 'hour_cos', 'hour_sc', 'week_sin', 'week_cos',
+            'is_weekend', 'month_sc', 'is_heatoffmonth', 'is_heatonmonth']
+
+X = data[features].astype(np.float32).values
+y = model.predict(X)
+y_real = scaler_y.inverse_transform(y).flatten()
+
+prediction = {
+    'date': data['datetime'].dt.date,
+    'time': data['datetime'].dt.time,
+    'tair': data['tair'],
+    'qai': y_real
+}
+
+with pd.ExcelWriter('result_ai_predisction.xlsx') as writer:
+    pd.DataFrame(prediction).to_excel(writer, sheet_name='Result', index=False)
+
+
 
